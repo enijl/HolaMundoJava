6bb4985140dd912e62962a1505ad4b32e3965f38
enijl <enijl1@miumg.edu.gt>
2025-02-28 13:31:01 -0600

    Se realzaron cambios en la estructura git push origin version1

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola git y git2 - Versión 2");
+        System.out.println("Hola, Mundo - Versión 1");
+        System.out.println("Este es un nuevo mensaje en la versión 1");
     }
 }
